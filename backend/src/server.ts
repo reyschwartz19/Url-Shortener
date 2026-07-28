ff487d28e6694130b24b1a563200c68f87956839
Rey <njongremy30@gmail.com>
2026-07-28 20:09:03 +0100

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,7 @@ const flusherHandle = startClickFlusher();
 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "http://localhost:3000"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
